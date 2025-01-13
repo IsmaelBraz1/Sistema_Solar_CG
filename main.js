25092bea76b4f853651ab442933ffabf23afefd4
IsmaelBraz <brazismael1@gmail.com>
2025-01-13 19:11:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-/*import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-//camera e cena
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.shadowMap.enabled = true;
-
-document.body.appendChild( renderer.domElement );
-
-//controle por mouse
-const orbit = new OrbitControls(camera, renderer.domElement)
-orbit.update()
-
-//cubo
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
-
-function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-
-}*/
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
